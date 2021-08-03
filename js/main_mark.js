546959f9e158aeb3af99c51ccfb8e58cb8f696a3
Mark Joseph Suria <msuria00@mylangara.ca>
2021-08-03 16:25:27 -0700

    index page changes

--- js/main_mark.js
+++ js/main_mark.js
@@ -580,6 +580,59 @@ $(function() {
     });
 });
 
+function getSchedule() {
+    $("#booking-text").trigger("click");
+}
+
+function getBooking() {
+
+
+    let newRow = (
+        "<div id='myModal' class='modal'>" +
+        "<div class='modal-content'>" +
+        "<span class='close'>&times;</span>" +
+        "<p>Some text in the Modal..</p>" +
+        "</div></div>"
+
+
+        `<button class = 'end-track' type='button' onclick='endLiveTracking("${booking_id}")'> End Tracking </button>` +
+        "</div>"
+    );
+
+    $(".headerImageSittter").append(newRow);
+
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+}
+
+
+
+
 
 let evntlstener;
 
@@ -587,6 +640,19 @@ evntlstener = document.getElementById('calendar_save');
 if (evntlstener) {
     evntlstener.addEventListener('click', saveSchedule);
 }
+
+evntlstener = document.getElementById('setBooking');
+if (evntlstener) {
+    evntlstener.addEventListener('click', getSchedule);
+}
+
+
+evntlstener = document.getElementById('booknow');
+if (evntlstener) {
+    evntlstener.addEventListener('click', getBooking);
+}
+
+
 /***********MOBILE NAV**************** */
 
 const nav = document.querySelector("#nav");
