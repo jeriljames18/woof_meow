121e62d3e99ead14f6b4724d6e64847d9f86ad21
Mark Joseph Suria <msuria00@mylangara.ca>
2021-08-05 15:55:18 -0700

    profile page update and set booking

--- js/main_mark.js
+++ js/main_mark.js
@@ -618,20 +618,24 @@ function saveSchedule() {
 
 /********************************************************************************************************/
 
-// $(function() {
-//     $('input[name="datetimes"]').daterangepicker({
-//         timePicker: true,
-//         startDate: moment().startOf('hour'),
-//         endDate: moment().startOf('hour').add(1, 'hour'),
-//         locale: {
-//             format: 'MM/DD/YY hh:mm A'
-//         }
-//     });
-// });
-
 function getSchedule() {
     $("#booking-text").trigger("click");
 }
+
+
+$(function() {
+    $("#booking-text").daterangepicker({
+        timePicker: true,
+        startDate: moment().startOf('hour'),
+        endDate: moment().startOf('hour').add(1, 'hour'),
+        locale: {
+            format: 'MM/DD/YY hh:mm A'
+        }
+    });
+});
+
+
+
 
 function getBooking() {
 
@@ -705,6 +709,32 @@ if (evntlstener) {
     evntlstener.addEventListener('click', gotoSearch);
 }
 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 /***********MOBILE NAV**************** */
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline';
-const OFFLINE_URL = '/offline.html';
+const OFFLINE_URL = 'offline.html';
 
 const urlsToCache = ["/index.html?source=pwa",
     "/",
@@ -32,7 +32,7 @@ const urlsToCache = ["/index.html?source=pwa",
 
 self.addEventListener('install', (event) => {
     event.waitUntil((async() => {
-        const cache = await caches.open(urlsToCache);
+        const cache = await caches.open(CACHE_NAME);
         // Setting {cache: 'reload'} in the new request will ensure that the response
         // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
         await cache.add(new Request(urlsToCache, { cache: 'reload' }));
@@ -67,13 +67,17 @@ self.addEventListener('fetch', (event) => {
                 const networkResponse = await fetch(event.request);
                 return networkResponse;
             } catch (error) {
-
+                // catch is only triggered if an exception is thrown, which is likely
+                // due to a network error.
+                // If fetch() returns a valid HTTP response with a response code in
+                // the 4xx or 5xx range, the catch() will NOT be called.
                 console.log('Fetch failed; returning offline page instead.', error);
 
-                const cache = await caches.open(urlsToCache);
+                const cache = await caches.open(CACHE_NAME);
                 const cachedResponse = await cache.match(OFFLINE_URL);
                 return cachedResponse;
             }
         })());
     }
+
 });
